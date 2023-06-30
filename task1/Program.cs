a4c42a948c1103ff22b85f72ca69be04cfa44648
maria <antonova.maria.a@gmail.com>
2023-06-30 15:35:26 +0300

    initial commit

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] Fillarray(string  text) {
+    System.Console.WriteLine(text);
+    int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    return array;}
+
+int Count(int[] arr){
+    int count = 0;
+    int i = 0;
+    while (i < arr.Length){
+        if (arr[i]>0) {count = count + 1;}
+        i++;}
+    return count;
+}
+
+int[] array = Fillarray("input array, split with space");
+System.Console.WriteLine(Count(array));
